026b14213cd09735d9fd768e0deed4a834213a33
yeezap <9rkrkrkrk9@gmail.com>
2024-12-07 20:23:00 -0500

    Create chart.js

---
+++ chart.js
@@ -0,0 +1,12 @@
+function toggle()
+{
+ object = document.getElementById("table")
+  if( object.className == "visible" )
+  {
+    object.className = "invisible";
+  }
+  else
+  {
+    object.className = "visible";
+  }
+}
