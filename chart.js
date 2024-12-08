1bf701bf78a85b01033e324c463c25f1b0c92dc0
yeezap <9rkrkrkrk9@gmail.com>
2024-12-07 20:08:26 -0500

    Create chart.js

---
+++ chart.js
@@ -0,0 +1,14 @@
+function toggle()
+{
+ object = document.getElementById("table")
+  if( object.className == "visible" )
+  {
+    object.className = "invisible";
+  }
+  else
+  {
+    object.className = "visible";
+  }
+
+
+}
